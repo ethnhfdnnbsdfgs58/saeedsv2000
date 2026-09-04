ad398d3dfbf976af67608c55d44cefb8e38ae493
ethnhfdnnbsdfgs58 <ethnhfdnnbsdfgs58@atomicmail.io>
2026-09-04 16:43:39 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 
 echo "🚀 Generating configuration..."
 cd /app
+
+# تست xray
+echo "Testing xray..."
+which xray
+xray version
+xray x25519
+
 python3 generate-config.py
 
 echo "🔧 Starting Xray..."
