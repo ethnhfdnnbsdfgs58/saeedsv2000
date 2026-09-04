6e25251d75c499026f7c9f2db1f2b6330bfa937b
ethnhfdnnbsdfgs58 <ethnhfdnnbsdfgs58@atomicmail.io>
2026-09-04 16:33:34 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "🚀 Generating configuration..."
-python3 /app/generate-config.py
+python3 generate-config.py  # ← مسیر رو عوض کن (بدون /app)
 
 echo "🔧 Starting Xray..."
 exec /usr/bin/xray -config /etc/xray/config.json
